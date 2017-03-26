eae9f56bd64bcb7d3512e0c2c7d4e93815dcc0e8
chizoba <ogbonnacj2@gmail.com>
2017-03-26 15:29:36 +0100

    ui: redesigned

--- app/src/main/java/com/github/chizoba/firebasecloudfunctions/MainActivity.java
+++ app/src/main/java/com/github/chizoba/firebasecloudfunctions/MainActivity.java
@@ -2,6 +2,9 @@ package com.github.chizoba.firebasecloudfunctions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void subscribeToTopic(View view) {
+        FirebaseMessaging.getInstance().subscribeToTopic("notifications");
+    }
+
+    public void sendMessage(View view) {
     }
 }
