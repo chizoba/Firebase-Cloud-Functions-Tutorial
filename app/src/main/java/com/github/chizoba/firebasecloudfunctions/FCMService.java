0fc16fe5a7f3fb68e13d50a4470b7e653b40921d
chizoba <ogbonnacj2@gmail.com>
2017-03-27 01:18:29 +0100

    fix: Added comments

--- app/src/main/java/com/github/chizoba/firebasecloudfunctions/FCMService.java
+++ app/src/main/java/com/github/chizoba/firebasecloudfunctions/FCMService.java
@@ -19,23 +19,14 @@ import com.google.firebase.messaging.RemoteMessage;
 public class FCMService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMsgService";
 
-    /**
-     * Called when message is received.
-     *
-     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-     */
-    // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
         // There are two types of messages data messages and notification messages. Data messages are handled
         // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
         // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
         // is in the foreground. When the app is in the background an automatically generated notification is displayed.
         // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
+        // and data payloads are treated as notification messages.
         String notificationTitle = null, notificationBody = null;
         String dataTitle = null, dataMessage = null;
 
@@ -61,13 +52,8 @@ public class FCMService extends FirebaseMessagingService {
         // message, here is where that should be initiated. See sendNotification method below.
         sendNotification(notificationTitle, notificationBody, dataTitle, dataMessage);
     }
-    // [END receive_message]
 
-    /**
-//     * Create and show a simple notification containing the received FCM message.
-//     *
-//     * @param messageBody FCM message body received.
-//     */
+    //  Create and show a simple notification containing the received FCM messages
     private void sendNotification(String notificationTitle, String notificationBody, String dataTitle, String dataMessage) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("title", dataTitle);
--- app/src/main/java/com/github/chizoba/firebasecloudfunctions/MainActivity.java
+++ app/src/main/java/com/github/chizoba/firebasecloudfunctions/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Handle possible data accompanying notification message.
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
                 if (key.equals("title")) {
